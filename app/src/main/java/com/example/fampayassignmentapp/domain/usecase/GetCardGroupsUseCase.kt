9db4f682e1b00eebe964692e0151620c2bb43fb0
aikansh_yadav <aikansh2001yadav@gmail.com>
2022-08-25 23:46:38 +0530

    fix design bug, change screenshots and apk

--- app/src/main/java/com/example/fampayassignmentapp/domain/usecase/GetCardGroupsUseCase.kt
+++ app/src/main/java/com/example/fampayassignmentapp/domain/usecase/GetCardGroupsUseCase.kt
@@ -9,13 +9,26 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
+
 class GetCardGroupsUseCase @Inject constructor(private val repository: FampayRepo) {
     operator fun invoke(): Flow<Resource<List<CardGroup>>> = flow {
         try {
             emit(Resource.Loading())
             val response = repository.getCardGroups()
             if (response.isSuccessful) {
-                emit(Resource.Success(response.body()!!.response!!))
+                val designTypesInSequence = arrayListOf<String>("HC3", "HC6", "HC5", "HC9", "HC1")
+                val cardGroupList = response.body()!!.response
+
+                val newCardGroupList = arrayListOf<CardGroup>()
+
+                for (designType in designTypesInSequence) {
+                    for (cardGroup in cardGroupList!!) {
+                        if (cardGroup.designType.equals(designType, ignoreCase = true)) {
+                            newCardGroupList.add(cardGroup)
+                        }
+                    }
+                }
+                emit(Resource.Success(newCardGroupList))
             } else {
                 emit(returnAuthOrServerErrorListResponse<List<CardGroup>>(response))
             }
